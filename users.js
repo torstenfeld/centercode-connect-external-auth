7dd2fae71046ebeb1d1f6b4a02e80fb49d4a02b6
Torsten Feld <torsten@torsten-feld.de>
2013-06-09 13:13:43 +0200

    made big users object instead of array of objects to speed up user search on big users list

--- users.js
+++ users.js
@@ -1,16 +1,19 @@
 
 
-var users = [{
+var users = {
+    "test": {
         "username": "test",
         "firstname": "Testfirst",
         "lastname": "Testlast",
         "email": "test@test.de",
         "password": "qwe123"
-    }, {
+    }, 
+    "test2": {
         "username": "test2",
         "firstname": "Testfirst2",
         "lastname": "Testlas2t",
         "email": "test2@test.de",
         "password": "qwe1234"
-    }];
+    }
+};
 module.exports = users;
