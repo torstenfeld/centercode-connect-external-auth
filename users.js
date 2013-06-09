477f386e34647a791b401978ef1ce4410c5c1e65
Torsten Feld <torsten@torsten-feld.de>
2013-06-09 12:48:44 +0200

    added list of users as external file for easier handling

---
+++ users.js
@@ -0,0 +1,16 @@
+
+
+var users = [{
+        "username": "test",
+        "firstname": "Testfirst",
+        "lastname": "Testlast",
+        "email": "test@test.de",
+        "password": "qwe123"
+    }, {
+        "username": "test2",
+        "firstname": "Testfirst2",
+        "lastname": "Testlas2t",
+        "email": "test2@test.de",
+        "password": "qwe1234"
+    }];
+module.exports = users;
